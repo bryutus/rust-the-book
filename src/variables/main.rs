ad60614af029ead87f72fe700bf0598590cc3a7f
Ryu Segawa <bryutus@gmail.com>
2023-01-21 11:14:12 +0900

    3-1 constants

--- src/variables/main.rs
+++ src/variables/main.rs
@@ -1,6 +1,10 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
     let mut x = 5;
     println!("The Value of x is {}", x);
     x = 6;
     println!("The Value of x is {}", x);
+
+    print!("The Value of MAX_POINTS is {}", MAX_POINTS);
 }
