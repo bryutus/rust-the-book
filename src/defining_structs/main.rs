3b40a0e53ccd68efb637f3117b4ab48f44a08ea3
Ryu Segawa <bryutus@gmail.com>
2023-02-04 22:34:44 +0900

    5.1 defining structs - using the field init shorthand

---
+++ src/defining_structs/main.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = build_user(String::from(""), String::from(""));
+    println!("{:?}", user2);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
