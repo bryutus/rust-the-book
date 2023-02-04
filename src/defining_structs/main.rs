ddbe3b3bca33fd5a3fc9658b26644162c76fa844
Ryu Segawa <bryutus@gmail.com>
2023-02-04 23:04:24 +0900

    5.1 defining structs - Struct Update Syntax, Tuple Structs

--- src/defining_structs/main.rs
+++ src/defining_structs/main.rs
@@ -6,6 +6,11 @@ struct User {
     active: bool,
 }
 
+#[derive(Debug)]
+struct Color(i32, i32, i32);
+#[derive(Debug)]
+struct Point(i32, i32, i32);
+
 fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -16,8 +21,31 @@ fn main() {
 
     user1.email = String::from("anotheremail@example.com");
 
-    let user2 = build_user(String::from(""), String::from(""));
+    let builded_user = build_user(String::from(""), String::from(""));
+    println!("{:?}", builded_user);
+
+    // Creating Instances from Other Instances with Struct Update Syntax
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
     println!("{:?}", user2);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+    println!("{:?}", user2);
+
+    // Using Tuple Structs Without Named Fields to Create Different Types
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("{:?}", black);
+    println!("{:?}", origin);
 }
 
 fn build_user(email: String, username: String) -> User {
