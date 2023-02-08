fbce14c068eed28bb4223c80bc9055fba7d9a15a
Ryu Segawa <bryutus@gmail.com>
2023-02-08 23:31:32 +0900

    6.2 match

---
+++ src/match/main.rs
@@ -0,0 +1,55 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc..
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let c = value_in_cents(Coin::Quarter(UsState::Alabama));
+    println!("coin is {} cents", c);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{:?}", six);
+    let none = plus_one(None);
+    println!("{:?}", none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
