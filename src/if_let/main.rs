7e937f84c327f82a82366b2e4d9aba4f920359d8
Ryu Segawa <bryutus@gmail.com>
2023-02-08 23:53:08 +0900

    6.3 if let

---
+++ src/if_let/main.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc..
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+    println!("count is {}", count);
+
+    let coin = Coin::Nickel;
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("count is {}", count);
+}
