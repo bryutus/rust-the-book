ee5410c5e8c1650081f20521354376764f695da9
Ryu Segawa <bryutus@gmail.com>
2023-02-11 11:33:07 +0900

    8.1 Vectors

--- src/vectors/main.rs
+++ src/vectors/main.rs
@@ -27,4 +27,30 @@ fn main() {
     // v.push(6); // cannot borrow `v` as mutable because it is also borrowed as immutable
 
     println!("The first element is: {}", first);
+
+    // Iterating over the Values in a Vector
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+    println!("{:?}", v);
+
+    // Using an Enum to Store Multiple Types
+    #[derive(Debug)]
+    enum SpreadSheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row: Vec<SpreadSheetCell> = vec![
+        SpreadSheetCell::Int(3),
+        SpreadSheetCell::Text(String::from("blue")),
+        SpreadSheetCell::Float(10.12),
+    ];
+
+    for c in row {
+        println!("{:?}", c);
+    }
 }
