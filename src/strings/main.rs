bb066c81321ff096a24d23e2841621a8daba49ae
Ryu Segawa <bryutus@gmail.com>
2023-02-13 23:41:03 +0900

    8.2 strings - Updating a String

---
+++ src/strings/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    let data = "initial contents";
+
+    let s = data.to_string();
+    println!("{}", s);
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+    println!("{}", s);
+
+    let s = String::from("initial contents");
+    println!("{}", s);
+
+    let hello = String::from("السلام عليكم");
+    println!("{}", hello);
+    let hello = String::from("Dobrý den");
+    println!("{}", hello);
+    let hello = String::from("Hello");
+    println!("{}", hello);
+    let hello = String::from("שָׁלוֹם");
+    println!("{}", hello);
+    let hello = String::from("नमस्ते");
+    println!("{}", hello);
+    let hello = String::from("こんにちは");
+    println!("{}", hello);
+    let hello = String::from("안녕하세요");
+    println!("{}", hello);
+    let hello = String::from("你好");
+    println!("{}", hello);
+    let hello = String::from("Olá");
+    println!("{}", hello);
+    let hello = String::from("Здравствуйте");
+    println!("{}", hello);
+    let hello = String::from("Hola");
+    println!("{}", hello);
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {}", s1);
+    println!("s2 is {}", s2);
+
+    let mut l = String::from("lo");
+    l.push('l');
+    println!("{}", l)
+}
